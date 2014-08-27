ed4365cc664011c4f62b2faa3219ad5d0e2877c5
Wim Munters <wim.munters@kuleuven.be>
2014-08-27 14:40:21 +0200

    Nz is now taken as input to load_BLfieldstat

--- loading/load_BLfieldstat.m
+++ loading/load_BLfieldstat.m
@@ -1,13 +1,11 @@
-function [BLst] = load_BLfieldstat(name,Nx,Ny)
+function [BLst] = load_BLfieldstat(name,Nx,Ny,Nz)
 
 % First load in data
 	BLd = load(name);
 	BLd = reshape(BLd',[size(BLd,1)*size(BLd,2) 1]);
 	
 % Start dividing and reshaping, put into structure
-    Nz = 80;
 	N = Nx*Ny*Nz;
-	
 	BLst.u = reshape(BLd(0*N+1:N),[Nx Ny Nz]);
 	BLst.v = reshape(BLd(1*N+1:2*N),[Nx Ny Nz]);
 	BLst.w = reshape(BLd(2*N+1:3*N),[Nx Ny Nz]);
@@ -19,10 +17,6 @@ function [BLst] = load_BLfieldstat(name,Nx,Ny)
 	BLst.vw = reshape(BLd(8*N+1:9*N),[Nx Ny Nz]);
 	BLst.p = reshape(BLd(9*N+1:10*N),[Nx Ny Nz]);
 	BLst.w_st = reshape(BLd(10*N+1:11*N),[Nx Ny Nz]);
-
-
-	
-	
 
 end
 	
