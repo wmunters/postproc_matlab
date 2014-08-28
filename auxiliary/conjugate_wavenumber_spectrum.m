c50e662a6b79a3a74cd5abdff229765b4fc24b46
Wim Munters <wim.munters@kuleuven.be>
2014-08-28 11:26:10 +0200

    Added functions which can read in the BLfield.dat file, complete the spectrum and transform it into a physical space velocity field.

---
+++ auxiliary/conjugate_wavenumber_spectrum.m
@@ -0,0 +1,36 @@
+function [field_comp] = conjugate_wavenumber_spectrum(field)
+%conjugate_wavenumber_spectrum - function for completing the spectral field representation
+%   Function will fill in the part of the spectrum which is not stored in the SP-wind code because we rely on the Hermitian symmetry of the Fourier transform of a real array, i.e. the spectral field does not contain the negative values in k_x
+%
+% Syntax:  [field_comp] = conjugate_wavenumber_spectrum(field)
+%
+% Inputs:
+%    field      - spectral field without negative k_x
+%
+% Outputs:
+%    field_comp - spectral field completed with negative k_x
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: load_BLfield
+
+% Author: Wim Munters, KU Leuven, Dept. of Mechanical Engineering
+% Email : wim.munters@kuleuven.be
+% Date  : 27 aug 2014
+
+
+    Nxh = size(field,1);
+    Nyh = size(field,2)/2+1;
+    Ny  = size(field,2);
+    Nz  = size(field,3);
+    
+    field_comp = zeros(Nxh-2,Ny,Nz);
+    % ky=0
+    field_comp(:,1,:) = conj(flipud(field(2:end-1,1,:)));
+    % ky>0
+    field_comp(:,2:Nyh-1,:) = conj(fliplr(flipud(field(2:end-1,Nyh+1:end,:))));
+    % ky<0
+    field_comp(:,Nyh+1:end,:) = conj(fliplr(flipud(field(2:end-1,2:Nyh-1,:))));
+    
---
+++ loading/load_BLfield.m
@@ -0,0 +1,76 @@
+function [BLfield,fieldr] = load_BLfield(name, Nx, Ny, Nz, Nl)
+%load_BLfield - function for loading in spectral field representation
+%
+% Syntax:  [BLfield, fieldr] = load_BLfield(name, Nx, Ny, Nz, Nl)
+%
+% Inputs:
+%    name       - name of the file, usually just 'BL_field.dat'
+%    Nx         - size of the field in x direction (as in NS.setup)
+%    Ny         - size of the field in y direction
+%    Nz         - size of the field in z direction
+%
+% Outputs:
+%    BLfield    - structure of complex arrays of size Nx/2 + 1, Ny, Nz
+%    fieldr     - structure of real arrays of size Nx, Ny, Nz
+%
+% Other m-files required: conjugate_wavenumber_spectrum.m
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: load_BLfieldstat, load_field
+
+% Author: Wim Munters, KU Leuven, Dept. of Mechanical Engineering
+% Email : wim.munters@kuleuven.be
+% Date  : 27 aug 2014
+
+%------------- BEGIN CODE --------------
+
+    Nxh = Nx/2 + 1;
+    Nyh = Ny/2 + 1;
+
+    field_dumm = load(name);
+    field_dummc = field_dumm(:,1) + 1i*field_dumm(:,2);
+   
+    % First of all, load in the data
+
+    % u     
+    start  = 1;
+    amount = Nxh*Ny*Nz;
+    BLfield.u = reshape(field_dummc(start:start+amount-1), [Nxh Ny Nz]);
+    
+    % v 
+    start = start+amount;
+    amount = Nxh*Ny*Nz;
+    BLfield.v = reshape(field_dummc(start:start+amount-1), [Nxh Ny Nz]);
+
+    % w 
+    start = start+amount;
+    amount = Nxh*Ny*(Nz-1);
+    BLfield.w = reshape(field_dummc(start:start+amount-1), [Nxh Ny Nz-1]);
+
+    % theta
+    if(Nl>3)
+        start = start+amount;
+        amount = Nxh*Ny*Nz;
+        BLfield.theta = reshape(field_dummc(start:start+amount-1), [Nxh Ny Nz]);
+    end
+
+    % Now transform it to the real domain
+
+    % First, fill up the missing wavenumber content (negative k_x), knowing that the complex field is Hermitian
+    u_comp = conjugate_wavenumber_spectrum(BLfield.u);
+    v_comp = conjugate_wavenumber_spectrum(BLfield.v);
+    w_comp = conjugate_wavenumber_spectrum(BLfield.w);
+    
+    BLfield.u = [BLfield.u; u_comp];
+    BLfield.v = [BLfield.v; v_comp];
+    BLfield.w = [BLfield.w; w_comp];
+    
+    scaling = 1/(Nx*Ny);
+
+    % Then,  perform the inverse Fourier transform and store in the fieldr structure
+    fieldr.u = abs(ifft2(BLfield.u))*scaling;
+    fieldr.v = abs(ifft2(BLfield.v))*scaling;
+    fieldr.w = abs(ifft2(BLfield.w))*scaling;
+
+%------------- END OF CODE --------------
