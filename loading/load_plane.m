e045e5935aba838e5892b9378020e425e8485731
Wim Munters <wim.munters@kuleuven.be>
2015-09-17 11:18:38 +0200

    Updated load_plane script to work with binary files instead of ASCII.

--- loading/load_plane.m
+++ loading/load_plane.m
@@ -1,6 +1,9 @@
 function [u] = load_plane(name,N1,N2)
 
-    u = load(name);
+%    u = load(name);
+
+    fileID = fopen(name);
+    u = fread(fileID,'real*8');
     u = reshape(u',[size(u,1)*size(u,2) 1]);
     if(N1*N2 ~= size(u,1))
         disp('File longer than expected..')
